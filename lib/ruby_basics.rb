91c0ac9d45cf9220fa4c4671191c3cb3f926cae9
Shemmyla Green <shemmyla@hotmail.com>
2019-04-22 00:19:05 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,35 @@
 def division(num1, num2)
+ puts 42 / 7
  
 end
 
 def assign_variable(value)
+  puts "Bob"
   
 end
 
 def argue
-  
+  puts "I'm mad!"
 end
 
-def greeting
+def greeting(greeting, name)
+  puts "Hello John!"
 
 end
 
 def return_a_value
+  value = "Nice"
+  puts value
 
 end
 
 def last_evaluated_value
+  value = "expert"
+  puts value
 
 end
 
-def pizza_party
+def pizza_party(pizza, "cheese")
+  put pizza_party
 
 end
